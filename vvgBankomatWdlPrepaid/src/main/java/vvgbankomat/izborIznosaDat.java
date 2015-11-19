de7d7cf43fdd7a31c7fa0b691c5946b9f9b82459
admin <>
2015-11-19 21:46:31 +0100

    {/vvgBankomatWdlPrepaid/src/main/java/vvgbankomat/izborIznosaDat.java}

--- vvgBankomatWdlPrepaid/src/main/java/vvgbankomat/izborIznosaDat.java
+++ vvgBankomatWdlPrepaid/src/main/java/vvgbankomat/izborIznosaDat.java
@@ -10,106 +10,25 @@ public class izborIznosaDat implements java.io.Serializable
 
    static final long serialVersionUID = 1L;
 
-   @org.kie.api.definition.type.Label("Sto")
-   private java.lang.Boolean sto;
-   @org.kie.api.definition.type.Label("dvijesto")
-   private java.lang.Boolean dvijesto;
-   @org.kie.api.definition.type.Label("Tristo")
-   private java.lang.Boolean tristo;
-   @org.kie.api.definition.type.Label("Petsto")
-   private java.lang.Boolean petsto;
-   @org.kie.api.definition.type.Label("Tisucu")
-   private java.lang.Boolean tisucu;
-   @org.kie.api.definition.type.Label("PetTisuca")
-   private java.lang.Boolean pettisuca;
-
    @org.kie.api.definition.type.Label("IzabranIznos")
-   private int izabranIznos;
+   private Double izabranIznos;
 
    public izborIznosaDat()
    {
    }
 
-   public java.lang.Boolean getSto()
-   {
-      return this.sto;
-   }
-
-   public void setSto(java.lang.Boolean sto)
-   {
-      this.sto = sto;
-   }
-
-   public java.lang.Boolean getDvijesto()
-   {
-      return this.dvijesto;
-   }
-
-   public void setDvijesto(java.lang.Boolean dvijesto)
-   {
-      this.dvijesto = dvijesto;
-   }
-
-   public java.lang.Boolean getTristo()
-   {
-      return this.tristo;
-   }
-
-   public void setTristo(java.lang.Boolean tristo)
-   {
-      this.tristo = tristo;
-   }
-
-   public java.lang.Boolean getPetsto()
-   {
-      return this.petsto;
-   }
-
-   public void setPetsto(java.lang.Boolean petsto)
-   {
-      this.petsto = petsto;
-   }
-
-   public java.lang.Boolean getTisucu()
-   {
-      return this.tisucu;
-   }
-
-   public void setTisucu(java.lang.Boolean tisucu)
-   {
-      this.tisucu = tisucu;
-   }
-
-   public java.lang.Boolean getPettisuca()
-   {
-      return this.pettisuca;
-   }
-
-   public void setPettisuca(java.lang.Boolean pettisuca)
-   {
-      this.pettisuca = pettisuca;
-   }
-
-   public int getIzabranIznos()
+   public java.lang.Double getIzabranIznos()
    {
       return this.izabranIznos;
    }
 
-   public void setIzabranIznos(int izabranIznos)
+   public void setIzabranIznos(java.lang.Double izabranIznos)
    {
       this.izabranIznos = izabranIznos;
    }
 
-   public izborIznosaDat(java.lang.Boolean sto, java.lang.Boolean dvijesto,
-         java.lang.Boolean tristo, java.lang.Boolean petsto,
-         java.lang.Boolean tisucu, java.lang.Boolean pettisuca, int izabranIznos)
+   public izborIznosaDat(java.lang.Double izabranIznos)
    {
-      this.sto = sto;
-      this.dvijesto = dvijesto;
-      this.tristo = tristo;
-      this.petsto = petsto;
-      this.tisucu = tisucu;
-      this.pettisuca = pettisuca;
       this.izabranIznos = izabranIznos;
    }
 
